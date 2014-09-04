b6b54b4a80c901c8fee4ca4586e430fe9fa2b13f
linoso <linoso@gmail.com>
2014-09-03 21:06:36 -0400

    Adding bash scripts  to start up and shut down

--- reader-start.sh
+++ reader-start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-java -cp /home/pi/ArduinoListener/src/ Runner
+java -cp /home/pi/ArduinoListener/src/ Runner  &> /dev/null
+
+pid=`ps aux | grep Runner | awk '{print $2}'`
+if pid != null; then
+ return  0;
+ else
+ return 1;
+fi
