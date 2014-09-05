63ba453b6230a5d7e0de101663d86e7268dad9eb
Lorenzo Pons <lorenzo.pons@hotmail.it>
2014-09-05 01:03:22 +0000

    commit from arduino

--- reader-start.sh
+++ reader-start.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-java -cp /home/pi/ArduinoListener/src/ Runner  &> /dev/null
+java -cp /opt/ArduinoListener/src/ Runner  &> /dev/null
 
 pid=`ps aux | grep Runner | awk '{print $2}'`
 if $pid != null; then
  echo $pid
  else
  echo "program did not start"
+ return 1;
 fi
